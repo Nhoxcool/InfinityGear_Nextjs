89ce2391d782061144e1c5278a8eac085e4b0a69
Kiên Chung Tuấn_20522111 <Chungtuan2077@gmail.com>
2024-04-06 23:01:39 +0700

    navbar + signup + logo

--- app/components/navbar/NavUi.tsx
+++ app/components/navbar/NavUi.tsx
@@ -1,17 +1,18 @@
-"use client";
-import Link from "next/link";
-import React from "react";
+'use client';
+import Link from 'next/link';
+import React from 'react';
 import {
   Navbar as MaterialNav,
   IconButton,
   Spinner,
-} from "@material-tailwind/react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
-import ProfileMenu from "../ProfileMenu";
-import { MobileNav } from "../MobileNav";
-import CartIcon from "../CartIcon";
-import { UserCircleIcon, ShoppingBagIcon } from "@heroicons/react/24/outline";
-import useAuth from "@hooks/useAuth";
+} from '@material-tailwind/react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import ProfileMenu from '../ProfileMenu';
+import { MobileNav } from '../MobileNav';
+import CartIcon from '../CartIcon';
+import { UserCircleIcon, ShoppingBagIcon } from '@heroicons/react/24/outline';
+import useAuth from '@hooks/useAuth';
+import Image from 'next/image';
 
 interface Props {
   cartItemsCount: number;
@@ -19,14 +20,14 @@ interface Props {
 
 export const menuItems = [
   {
-    href: "/profile",
-    icon: <UserCircleIcon className="h-4 w-4" />,
-    label: "My Profile",
+    href: '/profile',
+    icon: <UserCircleIcon className='h-4 w-4' />,
+    label: 'My Profile',
   },
   {
-    href: "/profile/orders",
-    icon: <ShoppingBagIcon className="h-4 w-4" />,
-    label: "Orders",
+    href: '/profile/orders',
+    icon: <ShoppingBagIcon className='h-4 w-4' />,
+    label: 'Orders',
   },
 ];
 
@@ -36,22 +37,30 @@ export default function NavUI({ cartItemsCount }: Props) {
 
   React.useEffect(() => {
     const onResize = () => window.innerWidth >= 960 && setOpen(false);
-    window.addEventListener("resize", onResize);
-    return () => window.removeEventListener("resize", onResize);
+    window.addEventListener('resize', onResize);
+    return () => window.removeEventListener('resize', onResize);
   }, []);
 
   return (
     <>
-      <MaterialNav className="mx-auto max-w-screen-xl px-4 py-2">
-        <div className="flex items-center justify-between text-blue-gray-900">
-          <Link
-            href="/"
-            className="mr-4 cursor-pointer py-1.5 lg:ml-2 font-semibold"
-          >
-            InfinityGear
-          </Link>
+      <MaterialNav className='mx-auto max-w-screen-xl px-4 py-2'>
+        <div className='flex items-center justify-between text-blue-gray-900'>
+          <div className='flex flex-row items-center'>
+            <Image
+              width={50}
+              height={50}
+              src='/image/logo_only.png'
+              alt='logo image'
+            />
+            <Link
+              href='/'
+              className='mr-4 cursor-pointer py-1.5 lg:ml-2 font-semibold'
+            >
+              InfinityGear
+            </Link>
+          </div>
 
-          <div className="hidden lg:flex gap-2 items-center">
+          <div className='hidden lg:flex gap-2 items-center'>
             <CartIcon cartItems={cartItemsCount} />
             {loggedIn ? (
               <ProfileMenu menuItems={menuItems} />
@@ -59,12 +68,12 @@ export default function NavUI({ cartItemsCount }: Props) {
               <Spinner />
             ) : (
               <>
-                <Link className="px-4 py-1" href="/auth/signin">
+                <Link className='px-4 py-1 hover:underline' href='/auth/signin'>
                   Sign in
                 </Link>
                 <Link
-                  className="bg-blue-500 text-white px-4 py-1 rounded"
-                  href="/auth/signup"
+                  className='bg-blue-500 text-white px-4 py-1 rounded hover:bg-blue-700'
+                  href='/auth/signup'
                 >
                   Sign up
                 </Link>
@@ -72,25 +81,25 @@ export default function NavUI({ cartItemsCount }: Props) {
             )}
           </div>
 
-          <div className="lg:hidden flex items-center space-x-2">
+          <div className='lg:hidden flex items-center space-x-2'>
             <CartIcon cartItems={cartItemsCount} />
 
             <IconButton
-              variant="text"
-              color="blue-gray"
-              className="lg:hidden"
+              variant='text'
+              color='blue-gray'
+              className='lg:hidden'
               onClick={() => setOpen(!open)}
             >
               {open ? (
-                <XMarkIcon className="h-6 w-6" strokeWidth={2} />
+                <XMarkIcon className='h-6 w-6' strokeWidth={2} />
               ) : (
-                <Bars3Icon className="h-6 w-6" strokeWidth={2} />
+                <Bars3Icon className='h-6 w-6' strokeWidth={2} />
               )}
             </IconButton>
           </div>
         </div>
       </MaterialNav>
-      <div className="lg:hidden">
+      <div className='lg:hidden'>
         <MobileNav
           menuItems={menuItems}
           onClose={() => setOpen(false)}
