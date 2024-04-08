44b336a5b2a725e2879be79996ad00256e193f45
Kiên Chung Tuấn_20522111 <Chungtuan2077@gmail.com>
2024-04-08 13:06:06 +0700

    kep

--- app/components/MobileNav.tsx
+++ app/components/MobileNav.tsx
@@ -37,12 +37,12 @@ interface Props {
 export const CategoryItems = [
   {
     href: '/PC',
-    icon: <ComputerDesktopIcon className='h-4 w-4' />,
+    icon: <ComputerDesktopIcon className="h-4 w-4" />,
     label: 'PC',
   },
   {
     href: '/Laptop',
-    icon: <CpuChipIcon className='h-4 w-4' />,
+    icon: <CpuChipIcon className="h-4 w-4" />,
     label: 'Laptop',
   },
 ];
@@ -58,21 +58,21 @@ export function MobileNav({ open, onClose, menuItems }: Props) {
   return (
     <>
       <Drawer open={open} onClose={onClose}>
-        <div className='mb-2 flex items-center justify-between p-4 z-50'>
-          <div className='flex flex-row items-center'>
+        <div className="mb-2 flex items-center justify-between p-4 z-50">
+          <div className="flex flex-row items-center">
             <Image
               width={50}
               height={50}
-              src='/image/logo_only.png'
-              alt='logo image'
+              src="/image/logo_only.png"
+              alt="logo image"
             />
-            <Typography variant='h5' color='blue-gray'>
+            <Typography variant="h5" color="blue-gray">
               InfinityGear
             </Typography>
           </div>
 
-          <IconButton variant='text' color='blue-gray' onClick={onClose}>
-            <XMarkIcon strokeWidth={2} className='h-5 w-5' />
+          <IconButton variant="text" color="blue-gray" onClick={onClose}>
+            <XMarkIcon strokeWidth={2} className="h-5 w-5" />
           </IconButton>
         </div>
         <List>
@@ -88,10 +88,10 @@ export function MobileNav({ open, onClose, menuItems }: Props) {
           })}
 
           {isAdmin && (
-            <Link href='/dashboard'>
+            <Link href="/dashboard">
               <ListItem onClick={onClose}>
                 <ListItemPrefix>
-                  <RectangleGroupIcon className='h-4 w-4' />
+                  <RectangleGroupIcon className="h-4 w-4" />
                 </ListItemPrefix>
                 Dashboard
               </ListItem>
@@ -109,26 +109,26 @@ export function MobileNav({ open, onClose, menuItems }: Props) {
               />
             }
           >
-            <ListItem className='p-0' selected={categoryDropdownOpen === true}>
+            <ListItem className="p-0" selected={categoryDropdownOpen === true}>
               <AccordionHeader
                 onClick={() => toggleCategoryDropdown()}
-                className='border-b-0 p-3'
+                className="border-b-0 p-3"
               >
                 <ListItemPrefix>
-                  <ArchiveBoxIcon className='h-5 w-5' />
+                  <ArchiveBoxIcon className="h-5 w-5" />
                 </ListItemPrefix>
-                <Typography color='blue-gray' className='mr-auto font-normal'>
+                <Typography color="blue-gray" className="mr-auto font-normal">
                   Product Category
                 </Typography>
               </AccordionHeader>
             </ListItem>
-            <AccordionBody className='py-1'>
-              <List className='p-0'>
+            <AccordionBody className="py-1">
+              <List className="p-0">
                 {CategoryItems.map(({ href, icon, label }) => {
                   return (
                     <Link key={href} href={href}>
                       <ListItem onClick={onClose}>
-                        <ChevronRightIcon strokeWidth={3} className='h-3 w-5' />
+                        <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
                         <ListItemPrefix>{icon}</ListItemPrefix>
                         {label}
                       </ListItem>
@@ -142,21 +142,21 @@ export function MobileNav({ open, onClose, menuItems }: Props) {
           {loggedIn ? (
             <ListItem>
               <ListItemPrefix>
-                <PowerIcon className='h-5 w-5' />
+                <PowerIcon className="h-5 w-5" />
               </ListItemPrefix>
               Sign Out
             </ListItem>
           ) : (
-            <div className='flex items-cente flex-col mt-10 gap-10'>
+            <div className="flex items-cente flex-col mt-10 gap-10">
               <Link
-                className='px-4 py-1 flex-1 text-center'
-                href='/auth/signin'
+                className="px-4 py-1 flex-1 text-center"
+                href="/auth/signin"
               >
                 Sign in
               </Link>
               <Link
-                className='bg-blue-500 text-white px-4 py-1 rounded flex-1 text-center focus:bg-blue-700'
-                href='/auth/signup'
+                className="bg-blue-500 text-white px-4 py-1 rounded flex-1 text-center focus:bg-blue-700"
+                href="/auth/signup"
               >
                 Sign up
               </Link>
