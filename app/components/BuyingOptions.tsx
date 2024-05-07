6c1c9a9d7fe96dc240a9ce8cac1113abb75f979a
Tú Trần <nhoxcool2002@gmail.com>
2024-05-08 03:28:04 +0700

    addToCartApi

--- app/components/BuyingOptions.tsx
+++ app/components/BuyingOptions.tsx
@@ -1,11 +1,19 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useTransition } from "react";
 import { Button } from "@material-tailwind/react";
 import CartCountUpdater from "@components/CartCountUpdater";
+import { useParams, useRouter } from "next/navigation";
+import useAuth from "../hooks/useAuth";
+import { toast } from "react-toastify";
 
 export default function BuyingOptions() {
   const [quantity, setQuantity] = useState(1);
+  const [isPending, startTransition] = useTransition();
+  const { product } = useParams();
+  const productId = product[1];
+  const { loggedIn } = useAuth();
+  const router = useRouter();
 
   const handleIncrement = () => {
     setQuantity((prevCount) => prevCount + 1);
@@ -16,6 +24,21 @@ export default function BuyingOptions() {
     setQuantity((prevCount) => prevCount - 1);
   };
 
+  const addToCart = async () => {
+    if (!productId) return;
+
+    if (!loggedIn) return router.push("/auth/signin");
+
+    const res = await fetch("/api/product/cart", {
+      method: "POST",
+      body: JSON.stringify({ productId, quantity }),
+    });
+
+    const { error } = await res.json();
+    if (!res.ok && error) toast.error(error);
+    toast.success("Add to cart successfully");
+  };
+
   return (
     <div className="flex items-center space-x-2">
       <CartCountUpdater
@@ -24,7 +47,14 @@ export default function BuyingOptions() {
         value={quantity}
       />
 
-      <Button variant="text" color="blue">
+      <Button
+        onClick={() => {
+          startTransition(async () => await addToCart());
+        }}
+        variant="text"
+        color="blue"
+        disabled={isPending}
+      >
         Add to Cart
       </Button>
       <Button color="amber" className="rounded-full">
