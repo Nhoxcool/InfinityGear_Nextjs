05a1102a900c97537e5d06f822e44d2711814e2f
Tú Trần <nhoxcool2002@gmail.com>
2024-05-11 02:46:33 +0700

    UpdateFeaturedProduct

--- app/components/Notification.tsx
+++ app/components/Notification.tsx
@@ -7,7 +7,7 @@ export default function Notification() {
   return (
     <ToastContainer
       position="bottom-center"
-      autoClose={5000}
+      autoClose={3000}
       hideProgressBar={false}
       newestOnTop={false}
       closeOnClick
