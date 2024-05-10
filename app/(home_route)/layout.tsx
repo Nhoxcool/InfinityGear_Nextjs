7b878eaee73dbc266c31cf1d29bf4a718749151f
Tú Trần <nhoxcool2002@gmail.com>
2024-05-11 04:48:42 +0700

    BackToTopButton

--- app/(home_route)/layout.tsx
+++ app/(home_route)/layout.tsx
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 import React, { ReactNode } from "react";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
+import { BackToTopButton } from "../components/BackToTopButton";
 
 interface Props {
   children: ReactNode;
@@ -15,6 +16,7 @@ export default async function HomeLayout({ children }: Props) {
         <Navbar />
         {children}
       </div>
+      <BackToTopButton />
       <Footer />
     </div>
   );
